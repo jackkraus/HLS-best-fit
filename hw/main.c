f6325be9a5442a4fdd212805871a9e4fe6fea4dc
Jack Kraus <akraus3@luc.edu>
2025-02-25 11:06:29 -0600

    I've been ignoring the lasts values

--- hw/main.c
+++ hw/main.c
@@ -25,6 +25,8 @@ int add_nums(const int tempxs[], const int tempys[], int tempN, int* sx, int* sy
 	*sxx = 0;
 	
 	//printf("Before LOOP: sx=%d, sy=%d, sxy=%d, sxx%d\n", sx,sy,sxy, sxx);
+	
+	printf("<== Starting add_nums LOOP ==>\n");
 	for(int i = 0; i < tempN; i++){
 		*sx += tempxs[i];
 		*sy += tempys[i];
@@ -33,7 +35,89 @@ int add_nums(const int tempxs[], const int tempys[], int tempN, int* sx, int* sy
 	}
 }
 
+/* func: calc_chi_squared()
+ *
+ * This function calculates the goodness of fit from each of the parameters: 
+ */
+double calc_chi_squared(const double a, const double b, const int tempxs[], const int tempys[], const int tempsigmas[]) {
+	double temp = 0;
+	double tempOverSig = 0;
+	double tempOverSigSquared = 0;
+	double gof = 0; // our goodness-of-fit
+	double term1 = 1/((double)tempN - 2); // invoked after loop
+	double term2 = 0; // adding tempOverSigSquared
+	
+	double x, y, sig;
 
+
+	printf("<== Starting calc_chi_squared LOOP ==>\n");
+	for(int i = 0; i < tempN; i++) {
+		x = (double)tempxs[i];
+		y = (double)tempys[i];
+		sig = (double)tempsigmas[i];
+
+		temp = y - a - b*x;
+		tempOverSig = temp/sig;
+		tempOverSigSquared = tempOverSig*tempOverSig; 
+		
+	        term2 += tempOverSigSquared;
+
+		// For debugging precision of variables 
+		// if(i<10) {
+		// 	printf("i=%d ; tempOverSigSquared=%.6f\n",i,tempOverSigSquared);
+		// 	printf("i=%d ; term2=%.6f\n",i,term2);
+		// }
+		
+		// Clear out temp values, jic	
+		temp = 0;
+		tempOverSig = 0; 
+		tempOverSigSquared = 0;		
+	}
+		
+	printf("--> calc_chi_squared: term1 = %.9f\n",term1);
+	printf("--> calc_chi_squared: term2 = %.9f\n",term2);
+	gof = term1*term2; 
+	
+	printf("--> calc_chi_squared: gof = %.9f\n",gof);
+	return gof; 
+}
+
+/* func: calc_distance()
+ *
+ * This function calculates the distance `f(a,b)`
+ */
+double calc_distance(const double a, const double b, const int tempxs[], const int tempys[]) {
+	double f = 0; 	
+	double temp = 0;
+	double tempSquared = 0;
+	
+	double x, y, sig;
+
+	printf("<== Starting calc_distance LOOP ==>\n");
+	for(int i = 0; i < tempN; i++) {
+		x = (double)tempxs[i];
+		y = (double)tempys[i];
+
+		temp = y - a - b*x;
+		tempSquared = temp*temp;
+
+		f += tempSquared;
+
+		// For debugging precision of variables 
+		// if(i<10) {
+		// 	printf("i=%d ; temp=%.6f\n",i,temp);
+		// 	printf("i=%d ; tempSquared=%.6f\n",i,tempSquared);
+		// 	printf("i=%d ; f=%.6f\n",i,f);
+		// }	
+		
+		// Clear out temp values, jic	
+		temp = 0;
+		tempSquared = 0;
+	}
+
+	printf("--> calc_distance: f(a,b) = %.9f\n",f);
+	return f;
+}
 
 /* func: fit()
  * 
@@ -54,21 +138,16 @@ void fit( int tempxs[], int tempys[], int tempsigmas[], int templasts[], int tem
 	b1 = 0; 
 	b2 = 0; 
 
-	printf("============================\n");	
-	printf("===========ADDING===========\n");	
-	printf("============================\n");	
-
-
 	//<-- Call 'add_nums()' function -->
 	//printf("After INIT: sy=%d\n",sy);
 	add_nums(tempxs, tempys, tempN, &sx, &sy, &sxy, &sxx); 
 	sx2 = sx*sx; 		
 	//printf("After add_nums: sx2=%d\n",sx2);
 	
-	printf("Sum of x's: %d\n", sx);
-	printf("Sum of y's: %d\n", sy);
-	printf("Sum of yx's: %d\n", sxy);
-	printf("Sum of xx's: %d\n", sxx);
+	//printf("Sum of x's: %d\n", sx);
+	//printf("Sum of y's: %d\n", sy);
+	//printf("Sum of yx's: %d\n", sxy);
+	//printf("Sum of xx's: %d\n", sxx);
 
 	// <-- Parameter 'a' calc --> 	
 	a1 = sy*sxx - sxy*sx;
@@ -87,54 +166,15 @@ void fit( int tempxs[], int tempys[], int tempsigmas[], int templasts[], int tem
 	printf("Best fit param a: %0.6f\n", a);	
 	printf("Best fit param b: %0.6f\n", b);	
 	
-	printf("==================================\n");	
-	printf("===========Dist/GoF Calc==========\n");	
-	printf("============================\n");
 
-
+	// <-- Call `calc_distance()` function --> 	
 	double f = 0; // our distance
-	double temp = 0;
-	double tempSquared = 0;
-
-	double tempOverSig = 0;
-	double tempOverSigSquared = 0;
-	double gof = 0; // our goodness-of-fit
-	double term1 = 1/((double)tempN - 2); // invoked after loop
-	double term2 = 0; // adding tempOverSigSquared
+	f = calc_distance(a,b, tempxs, tempys);
 	
-	double x, y, sig;
-	for(int i = 0; i < tempN; i++) {
-		x = (double)tempxs[i];
-		y = (double)tempys[i];
-		sig = (double)tempsigmas[i];
-
-		temp = y - a - b*x;
-		tempSquared = temp*temp;
-		tempOverSig = temp/sig;
-		tempOverSigSquared = tempOverSig*tempOverSig; 
-		
-		f += tempSquared;
-	        term2 += tempOverSigSquared;
-
-		// For debugging precision of variables 
-		if(i<10) {
-			//printf("i=%d ; temp=%.6f\n",i,temp);
-			//printf("i=%d ; tempOverSig=%.6f\n",i,tempOverSig);
-			printf("i=%d ; tempOverSigSquared=%.6f\n",i,tempOverSigSquared);
-			printf("i=%d ; term2=%.6f\n",i,term2);
-			//printf("i=%d ; f=%.6f\n",i,f);
-		}
-		
-		// Clear out temp values, jic	
-		temp = 0;
-		tempSquared = 0;
-		tempOverSig = 0; 
-		tempOverSigSquared = 0;		
-	}
-		
-	printf("term1 = %.9f\n",term1);
-	printf("term2 = %.9f\n",term2);
-	gof = term1*term2; 
+	// <-- Call `calc_chi_squared()` function --> 	
+	double gof = 0; // our goodness of fit
+	gof = calc_chi_squared(a,b, tempxs, tempys, tempsigmas);
+	
 
 	printf("f(a,b) = %.9f\n",f);
 	printf("goodness of fit = %.9f\n",gof);
@@ -151,6 +191,13 @@ int main() {
        	//int lasts[] = {0,0,0,1,0};	  
 	//fit(array1, array2, sigmas, lasts, N);	
 	
+
+	//Eventually we'll have multiple events, instances where there's a set of x's and y's
+	//	- The way to distinguish an event from another is by the 'last' value. 
+	//		If the last value == 1, then that's the last x,y, and sigma for that event
+	//			So implement a check
+	//	- I have to create new arrays that take in the temp arguments and create new arrays 
+	//		by which it can then send in for the fit function
 	
 	fit(tempxs, tempys, tempsigmas, templasts, tempN);
 	
